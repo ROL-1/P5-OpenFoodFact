a0cea5618012c7f0a8147c5101fa373a83f21ea0
ROL-1 <loicromero1@gmail.com>
2020-07-19 22:45:06 +0200

    bdd communication(controller) : insert data = OK !

---
+++ controller/api_config.py
@@ -0,0 +1,8 @@
+""" config file for api.py"""
+
+# page=x
+research_params = [
+    'search_terms=butter',
+    'json=1',
+    'fields=product_name,nutriscore_grade,stores,brands,code,categories',
+]
